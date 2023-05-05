7abe960830019e8b1933888edc2a20b2fe8bbf09
ezequiel <ezequielvieira.77@gmail.com>
2023-05-04 21:28:24 -0300

    teste operadores e comandos variados

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,85 @@
 //CODIGO AQUI
+
+let saldo = 15
+saldo = saldo + 22.40
+console.log(saldo)
+
+saldo = saldo - 12.34
+console.log(saldo)
+
+saldo = saldo / 2
+console.log(saldo)
+
+saldo = saldo * 3.4
+//outra forma do mesmo calculo
+//saldo *= 3.4
+console.log(saldo)
+
+//sinal de modulo
+saldo = saldo % 3
+//saldo =% 3
+console.log(saldo)
+
+// ================================
+
+let operacao =2*(2+2)
+console.log(operacao)
+
+// ================================
+
+// se trata do programa pergunatando a semelhança
+// true
+const comparacao = 5 === 5
+console.log(comparacao)
+
+// false
+const comparacao2 = 5 !== 5
+console.log(comparacao2)
+
+// false
+const comparacao3 = 5 != "5"
+console.log(comparacao3)
+
+// false
+const comparacao4 = "5" === "cinco"
+console.log(comparacao4)
+
+// true
+const comparacao5 = typeof 5 === typeof 20
+console.log(comparacao5)
+
+// true
+const comparacao6 = typeof "5" === typeof "cinco"
+console.log(comparacao6)
+
+// ======================================
+
+// operadores 
+
+const premeiroNumero = 50
+const segundoNumero = 75
+
+const verifica = premeiroNumero === segundoNumero
+// false
+console.log(verifica)
+
+// true
+const verifica2 = premeiroNumero <= segundoNumero
+console.log(verifica2)
+
+// false
+const verifica3 = premeiroNumero > segundoNumero
+console.log(verifica3)
+
+// true
+const verifica4 = premeiroNumero < segundoNumero
+console.log(verifica4)
+
+
+
+
+
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,35 @@
 // EXERCICIO DE FIXAÇÃO
+let nome = prompt("qual seu nome")
+let nome2 = prompt("nome de seu amigo")
+let idade = prompt("sua idade")
+let idade2 = prompt("idade de seu amigo")
+console.log("ola seu nome por favor", nome,"obrigado", nome, "agora de seu amigo", nome2, "sua idade", idade, "e de  seu amigo", idade2)
+console.log("fique a vontade, só confirme seus dados,voce", nome, "seu amigo", nome2)
+console.log(nome)
+console.log(nome2)
+console.log(idade)
+console.log(idade2)
+
+const idade3 = 33
+const idade4 = 34
+
+const verifica = idade3 === idade4
+console.log(verifica)
+
+const verifica2 = idade3 > idade4
+console.log(verifica2)
+
+const verifica3 = idade3 < idade4
+console.log(verifica3)
+
+ const verifica4 = idade3 != idade4
+ console.log(verifica4)
+
+
+let idade5 =Number(prompt("consirme sua idade"))
+let idade6 =Number(prompt("confirme a idade do amigo"))
+
+console.log("aqui mostra que voce é mais novo,voce", idade3, "e ele", idade4)
+console.log("soma dos dois", idade5 + idade6, )
+console.log("diferença", idade5 - idade6)
 
